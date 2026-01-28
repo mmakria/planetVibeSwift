a652ce22f805b34dbc05476025822390dd44ca81
momomak <m.makria@gmail.com>
2026-01-28 15:49:43 +0100

    Initial Commit

---
+++ planetvibe/planetvibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  planetvibeApp.swift
+//  planetvibe
+//
+//  Created by Mohamed Makria on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct planetvibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
